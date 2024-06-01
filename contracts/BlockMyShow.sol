80b2987839eb0bbd19756e6bca05480ef77dfc80
ayushgupta2959 <ayushgupta2959@gmail.com>
2024-06-01 21:05:10 +0530

    added listing event/occasion on blockmyshow

--- contracts/BlockMyShow.sol
+++ contracts/BlockMyShow.sol
@@ -1,4 +1,56 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract BlockMyShow {}
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract BlockMyShow is ERC721 {
+    address public owner;
+    uint256 public totalOccasions;
+
+    struct Occasion {
+        uint256 id;
+        string name;
+        uint256 cost;
+        uint256 tickets;
+        uint256 maxTickets;
+        string date;
+        string time;
+        string location;
+    }
+
+    mapping(uint256 => Occasion) occasions;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {
+        owner = msg.sender;
+    }
+
+    function list(
+        string memory _name,
+        uint256 _cost,
+        uint256 _maxTickets,
+        string memory _date,
+        string memory _time,
+        string memory _location
+    ) public onlyOwner {
+        totalOccasions++;
+        occasions[totalOccasions] = Occasion(
+            totalOccasions,
+            _name,
+            _cost,
+            _maxTickets,
+            _maxTickets,
+            _date,
+            _time,
+            _location
+        );
+    }
+
+    function getOccasion(uint256 _id) public view returns (Occasion memory) {
+        return occasions[_id];
+    }
+}
--- test/BlockMyShow.js
+++ test/BlockMyShow.js
@@ -1,3 +1,69 @@
 const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
-describe("BlockMyShow", () => {});
+const NAME = "BlockMyShow";
+const SYMBOL = "BMS";
+
+const OCCASION_NAME = "ETH Texas";
+const OCCASION_COST = ethers.utils.parseUnits("1", "ether");
+const OCCASION_MAX_TICKETS = 100;
+const OCCASION_DATE = "Apr 27";
+const OCCASION_TIME = "10:00AM CST";
+const OCCASION_LOCATION = "Austin, Texas";
+
+describe("BlockMyShow", () => {
+  let blockMyShow;
+  let deployer, buyer;
+
+  beforeEach(async () => {
+    [deployer, buyer] = await ethers.getSigners();
+
+    const BlockMyShow = await ethers.getContractFactory("BlockMyShow");
+    blockMyShow = await BlockMyShow.deploy(NAME, SYMBOL);
+
+    const transaction = await blockMyShow
+      .connect(deployer)
+      .list(
+        OCCASION_NAME,
+        OCCASION_COST,
+        OCCASION_MAX_TICKETS,
+        OCCASION_DATE,
+        OCCASION_TIME,
+        OCCASION_LOCATION
+      );
+
+    await transaction.wait();
+  });
+
+  describe("Deployment", () => {
+    it("Sets the name", async () => {
+      expect(await blockMyShow.name()).to.equal(NAME);
+    });
+
+    it("Sets the symbol", async () => {
+      expect(await blockMyShow.symbol()).to.equal(SYMBOL);
+    });
+
+    it("Sets the owner", async () => {
+      expect(await blockMyShow.owner()).to.equal(deployer.address);
+    });
+  });
+
+  describe("Occasions", () => {
+    it("Returns occasions attributes", async () => {
+      const occasion = await blockMyShow.getOccasion(1);
+      expect(occasion.id).to.be.equal(1);
+      expect(occasion.name).to.be.equal(OCCASION_NAME);
+      expect(occasion.cost).to.be.equal(OCCASION_COST);
+      expect(occasion.tickets).to.be.equal(OCCASION_MAX_TICKETS);
+      expect(occasion.date).to.be.equal(OCCASION_DATE);
+      expect(occasion.time).to.be.equal(OCCASION_TIME);
+      expect(occasion.location).to.be.equal(OCCASION_LOCATION);
+    });
+
+    it("Updates occasions count", async () => {
+      const totalOccasions = await blockMyShow.totalOccasions();
+      expect(totalOccasions).to.be.equal(1);
+    });
+  });
+});
